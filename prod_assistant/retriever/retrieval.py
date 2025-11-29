18b169126ad2fd107f914d8422feabd1ac9c4112
advik257 <Adviktest99@gmail.com>
2025-11-29 16:23:26 +0530

    retrieval from AstraDB done

--- prod_assistant/retriever/retrieval.py
+++ prod_assistant/retriever/retrieval.py
@@ -11,25 +11,64 @@ class Retriever:
     """Base class for all retrievers."""
     
     def __init__(self):
-        pass
+        self.model_loader=ModelLoader()
+        self._load_env_varaibles()
+        self.config=load_config()
+        self.vector_store = None
+        self.retriever = None
     
     def _load_env_varaibles(self):
-        pass
+        
+        load_dotenv()
+        
+        required_vars = ["COHERE_API_KEY", "ASTRA_DB_API_ENDPOINT", "ASTRA_DB_APPLICATION_TOKEN", "ASTRA_DB_KEYSPACE"]
+        
+        missing_vars = [var for var in required_vars if os.getenv(var) is None]
+        if missing_vars:
+            raise EnvironmentError(f"Missing environment variables: {missing_vars}")
+        
+        self.cohere_api_key = os.getenv("COHERE_API_KEY")
+        self.db_api_endpoint = os.getenv("ASTRA_DB_API_ENDPOINT")
+        self.db_application_token = os.getenv("ASTRA_DB_APPLICATION_TOKEN")
+        self.db_keyspace = os.getenv("ASTRA_DB_KEYSPACE")
     
     
     def load_retriever(self):
-        pass
+        """
+        Load the retriever from AstraDB vector store.
+        """
+        if not self.vector_store:
+            
+            collection_name=self.config["astra_db"]["collection_name"]
+            
+            self.vector_store = AstraDBVectorStore(
+                embedding= self.model_loader.load_embeddings(),
+                collection_name=collection_name,
+                api_endpoint=self.db_api_endpoint,
+                token=self.db_application_token,
+                namespace=self.db_keyspace
+            )        
+            
+            top_k = self.config["retriever"]["top_k"]
+            self.retriever = self.vector_store.as_retriever(search_kwargs={"k": top_k})
+            
+            return self.retriever
+        
     
-    
-    
-    def call_retriever(self):
-        pass
+    def call_retriever(self, query):
+        
+        """ call the retriever to get relevant documents."""
+        
+        retriever = self.load_retriever()
+        output = retriever.invoke(query)
+        
+        return output
     
 
 if __name__ == "__main__":
     retriever = Retriever()
     
-    user_query ="can you suggest good budget loptop?"
+    user_query ="can you suggest good budget phones?"
     results = retriever.call_retriever(user_query)
     for doc in results:
         print(doc.page_content)
